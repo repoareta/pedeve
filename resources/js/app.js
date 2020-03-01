1409d8d588021ff175661bd74767e8238828832f
erkalodon <andhika.ragilkesuma@gmail.com>
2020-03-01 15:36:09 +0700

    Metronic Theme Integration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,4 @@
-require('./bootstrap');
+// require('./bootstrap');
+
+import '../demo8/tools/webpack/vendors/global';
+import '../demo8/tools/webpack/scripts';
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,20 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+// copy images folder into laravel public folder
+mix.copyDirectory('resources/demo8/src/assets/media', 'public/assets/media');
+
+/**
+ * plugins specific issue workaround for webpack
+ * @see https://github.com/morrisjs/morris.js/issues/697
+ * @see https://stackoverflow.com/questions/33998262/jquery-ui-and-webpack-how-to-manage-it-into-module
+ */
+mix.webpackConfig({
+    resolve: {
+        alias: {
+            'morris.js': 'morris.js/morris.js',
+            'jquery-ui': 'jquery-ui',
+        },
+    },
+});
