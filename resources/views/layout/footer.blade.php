2b7fcf6204fb7163ac576598124a804bc812aee6
erkalodon <andhika.ragilkesuma@gmail.com>
2020-03-16 02:31:54 +0700

    Update layouting add quick panel

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,4 +1,4 @@
-<!-- begin:: Footer -->
+
 <div class="kt-footer  kt-grid__item kt-grid kt-grid--desktop kt-grid--ver-desktop" id="kt_footer">
     <div class="kt-container  kt-container--fluid ">
         <div class="kt-footer__copyright">
@@ -11,4 +11,3 @@
         </div>
     </div>
 </div>
-<!-- end:: Footer -->
