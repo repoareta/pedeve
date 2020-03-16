0df89ebefb5690916b0171c9da197e339e649ad7
erkalodon <andhika.ragilkesuma@gmail.com>
2020-03-16 16:13:49 +0700

    disable permission temporary

--- routes/web.php
+++ routes/web.php
@@ -24,24 +24,25 @@ Route::get('/logout', 'AuthController@logout');
 // UMK
 // Route::get('/', 'UmumUmkController@index');
 
-// Perjalanan Dinas
 Route::group(['middleware'=>['auth','checkRole:admin']], function () {
-    Route::prefix('umum')->group(function () {
-        // Matches The "/umum/perjalanan_dinas" URL
-        // Route assigned name "umum.perjalanan_dinas"...
-        // Perjalanan Dinas
-        Route::get('perjalanan_dinas', 'PerjalananDinasController@index')->name('perjalanan_dinas.index');
-        Route::get('perjalanan_dinas/create', 'PerjalananDinasController@create')->name('perjalanan_dinas.create');
-        Route::get('perjalanan_dinas/edit', 'PerjalananDinasController@edit')->name('perjalanan_dinas.edit');
+});
+
+// Perjalanan Dinas
+Route::prefix('umum')->group(function () {
+    // Matches The "/umum/perjalanan_dinas" URL
+    // Route assigned name "umum.perjalanan_dinas"...
+    // Perjalanan Dinas
+    Route::get('perjalanan_dinas', 'PerjalananDinasController@index')->name('perjalanan_dinas.index');
+    Route::get('perjalanan_dinas/create', 'PerjalananDinasController@create')->name('perjalanan_dinas.create');
+    Route::get('perjalanan_dinas/edit', 'PerjalananDinasController@edit')->name('perjalanan_dinas.edit');
 
 
-        // Permintaan Bayar
-        Route::get('permintaan_bayar', 'PermintaanBayarController@index')->name('permintaan_bayar.index');
-        
-        // Anggaran
-        Route::get('anggaran', 'AnggaranController@index')->name('anggaran.index');
-        
-        // Report UMUM
-        Route::get('report', 'ReportController@index')->name('report.index');
-    });
+    // Permintaan Bayar
+    Route::get('permintaan_bayar', 'PermintaanBayarController@index')->name('permintaan_bayar.index');
+    
+    // Anggaran
+    Route::get('anggaran', 'AnggaranController@index')->name('anggaran.index');
+    
+    // Report UMUM
+    Route::get('report', 'ReportController@index')->name('report.index');
 });
