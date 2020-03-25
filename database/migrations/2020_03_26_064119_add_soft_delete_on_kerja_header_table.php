5105785e1b4a7a94aea00a00fc6d109608f4b941
mulyono <37521871+mulyonomuiz63@users.noreply.github.com>
2020-03-26 06:44:20 +0700

    restor tabel kerja_header

--- database/migrations/2020_03_26_064119_add_soft_delete_on_kerja_header_table.php
+++ database/migrations/2020_03_26_064119_add_soft_delete_on_kerja_header_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatTblKerjaHeader extends Migration
+class AddSoftDeleteOnKerjaHeaderTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreatTblKerjaHeader extends Migration
      */
     public function up()
     {
-        Schema::table('kerja_header', function (Blueprint $table) {
-            //
-        $table->string('kepadaa')->nullable()->after('jumlah');
+        Schema::table('kerja_header', function ($table) {
+            $table->softDeletes();
         });
     }
 
@@ -26,8 +25,8 @@ class CreatTblKerjaHeader extends Migration
      */
     public function down()
     {
-        Schema::table('kerja_header', function (Blueprint $table) {
-            //
+        Schema::table("kerja_header", function ($table) {
+            $table->dropSoftDeletes();
         });
     }
 }
