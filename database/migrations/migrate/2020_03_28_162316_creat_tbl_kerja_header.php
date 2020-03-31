c100db949d7ba347a6607b848fc349fe25536dfa
erkalodon <andhika.ragilkesuma@gmail.com>
2020-03-31 10:41:04 +0700

    update migrate kerja header

--- database/migrations/migrate/2020_03_28_162316_creat_tbl_kerja_header.php
+++ database/migrations/migrate/2020_03_28_162316_creat_tbl_kerja_header.php
@@ -13,9 +13,24 @@ class CreatTblKerjaHeader extends Migration
      */
     public function up()
     {
-        // Schema::create('kerja_header', function (Blueprint $table) {
-        //     $table->string('kepada', 200); => nambah colom kepada
-        // });
+        Schema::create('kerja_header', function (Blueprint $table) {
+            $table->timestamp('tgl_panjar');
+            $table->string('app_sdm',1);
+            $table->string('app_sdm_oleh',15);
+            $table->timestamp('app_sdm_tgl');
+            $table->string('app_pbd_oleh',15);
+            $table->timestamp('app_pbd_tgl');
+            $table->string('no_kas',25);
+            $table->string('bulan_buku',6);
+            $table->string('keterangan',200);
+            $table->string('ci',1);
+            $table->string('app_pbd',1);
+            $table->bigInteger('rate');
+            $table->string('jenis_um',1);
+            $table->string('no_umk',25)->primary();
+            $table->bigInteger('jumlah');        
+            $table->string('kepada', 200);
+        });
     }
 
     /**
@@ -25,6 +40,6 @@ class CreatTblKerjaHeader extends Migration
      */
     public function down()
     {
-        // Schema::dropIfExists('kerja_header');
+        Schema::dropIfExists('kerja_header');
     }
 }
