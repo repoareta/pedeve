163c5f9f55b6ca8dd037cb6e8357afb68328d017
erkalodon <andhika.ragilkesuma@gmail.com>
2020-03-31 14:40:29 +0700

    Fix update table migrations

--- database/migrations/2020_03_28_162135_create_tbl_vendor.php
+++ database/migrations/2020_03_28_162135_create_tbl_vendor.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatTblVendor extends Migration
+class CreateTblVendor extends Migration
 {
     /**
      * Run the migrations.
@@ -17,7 +17,7 @@ class CreatTblVendor extends Migration
             $table->increments('vendorid');
             $table->string('nama', 100);
             $table->text('alamat');
-            $table->string('telpon',20);
+            $table->string('telpon', 20);
         });
     }
 
