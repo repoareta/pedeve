c202a1a8868198952934a44af377ffb6d29b8b23
mulyono <37521871+mulyonomuiz63@users.noreply.github.com>
2020-03-31 07:31:06 +0700

    migration

--- database/migrations/2020_03_28_162316_creat_tbl_kerja_header.php
+++ database/migrations/2020_03_28_162316_creat_tbl_kerja_header.php
@@ -13,9 +13,23 @@ class CreatTblKerjaHeader extends Migration
      */
     public function up()
     {
-        Schema::table('kerja_header', function (Blueprint $table) {
-            //
-        $table->string('kepadaa')->nullable()->after('jumlah');
+        Schema::create('kerja_header', function (Blueprint $table) {
+            $table->timestamp('tgl_panjar');
+            $table->string('app_sdm',1);
+            $table->string('app_sdm_oleh',15);
+            $table->timestamp('app_sdm_tgl');
+            $table->string('app_pbd_oleh',15);
+            $table->timestamp('app_pbd_tgl');
+            $table->string('no_kas',25);
+            $table->string('bulan_buku',6);
+            $table->string('keterangan',200);
+            $table->string('ci',1);
+            $table->string('app_pbd',1);
+            $table->bigInteger('rate');
+            $table->string('jenis_um',1);
+            $table->string('no_umk',25)->primary();
+            $table->bigInteger('jumlah');        
+            $table->string('kepada', 200);
         });
     }
 
@@ -26,8 +40,6 @@ class CreatTblKerjaHeader extends Migration
      */
     public function down()
     {
-        Schema::table('kerja_header', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('kerja_header');
     }
 }
