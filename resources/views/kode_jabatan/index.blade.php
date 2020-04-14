013599f71682ee855b5fac9536d632bb04c9b993
erkalodon <andhika.ragilkesuma@gmail.com>
2020-04-15 02:54:50 +0700

    fix breadcrumbs

--- resources/views/kode_jabatan/index.blade.php
+++ resources/views/kode_jabatan/index.blade.php
@@ -12,7 +12,8 @@
 				<a href="#" class="kt-subheader__breadcrumbs-home"><i class="flaticon2-shelter"></i></a>
 				<span class="kt-subheader__breadcrumbs-separator"></span>
 				<a href="" class="kt-subheader__breadcrumbs-link">
-					SDM & Payroll </a>
+					SDM & Payroll
+				</a>
 				<span class="kt-subheader__breadcrumbs-separator"></span>
 				<span class="kt-subheader__breadcrumbs-link kt-subheader__breadcrumbs-link--active">Kode Jabatan</span>
 			</div>
