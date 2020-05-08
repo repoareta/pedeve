5b406c4bb41dd464c4065c7ba9f43ae80966593d
erkalodon <andhika.ragilkesuma@gmail.com>
2020-05-08 08:00:47 +0700

    Fix index filter and search Hutang Master

--- app/Models/HutangMaster.php
+++ app/Models/HutangMaster.php
@@ -10,7 +10,7 @@ class HutangMaster extends Model
     use CompositeKey;
     
     protected $table = "pay_master_hutang";
-    protected $primaryKey = ['tahun', 'bulan', 'nopek'];
+    protected $primaryKey = ['tahun', 'bulan', 'nopek', 'aard'];
     protected $keyType = 'string';
     public $timestamps = false;
     public $incrementing = false;
