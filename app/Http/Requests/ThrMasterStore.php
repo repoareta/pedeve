a6628adeca469c3e7aabf86f952a769936bc744b
erkalodon <andhika.ragilkesuma@gmail.com>
2020-05-13 00:34:36 +0700

    Fix create and edit Thr Master

---
+++ app/Http/Requests/ThrMasterStore.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ThrMasterStore extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "bulan"          => "required",
+            "tahun"          => "required|numeric|digits:4",
+            "pegawai"        => "required",
+            "aard"           => "required",
+            "jumlah_cicilan" => "required",
+            "cicilan"        => "required",
+            "nilai"          => "required",
+        ];
+    }
+}
--- resources/views/thr_master/create.blade.php
+++ resources/views/thr_master/create.blade.php
@@ -6,7 +6,7 @@
 	<div class="kt-container  kt-container--fluid ">
 		<div class="kt-subheader__main">
 			<h3 class="kt-subheader__title">
-				Master Upah </h3>
+				Master THR </h3>
 			<span class="kt-subheader__separator kt-hidden"></span>
 			<div class="kt-subheader__breadcrumbs">
 				<a href="#" class="kt-subheader__breadcrumbs-home"><i class="flaticon2-shelter"></i></a>
@@ -16,9 +16,9 @@
 				</a>
 				<span class="kt-subheader__breadcrumbs-separator"></span>
 				<a href="" class="kt-subheader__breadcrumbs-link">
-					Master Upah </a>
+					Master THR </a>
 				<span class="kt-subheader__breadcrumbs-separator"></span>
-				<span class="kt-subheader__breadcrumbs-link kt-subheader__breadcrumbs-link--active">Ubah</span>
+				<span class="kt-subheader__breadcrumbs-link kt-subheader__breadcrumbs-link--active">Tambah</span>
 			</div>
 		</div>
 	</div>
@@ -33,7 +33,7 @@
 					<i class="kt-font-brand flaticon2-plus-1"></i>
 				</span>
 				<h3 class="kt-portlet__head-title">
-					Ubah Master Upah
+					Tambah Master THR
 				</h3>
 			</div>
 			<div class="kt-portlet__head-toolbar">
@@ -42,12 +42,7 @@
 			</div>
 		</div>
 			<div class="kt-portlet__body">
-				<form class="kt-form kt-form--label-right" id="formUpahMaster" action="{{ route('upah.update', [
-					'tahun' => $upah->tahun, 
-					'bulan' => $upah->bulan, 
-					'nopek'=> $upah->nopek, 
-					'aard'=> $upah->aard, 
-				]) }}" method="POST">
+				<form class="kt-form kt-form--label-right" id="formThrMaster" action="{{ route('thr.store') }}" method="POST">
 					@csrf
 
 					<div class="form-group row">
@@ -55,25 +50,25 @@
 						<div class="col-4">
 							<select class="form-control kt-select2" name="bulan" id="bulan">
 								<option value="">- Pilih Bulan -</option>
-								<option value="1" @if($upah->bulan == '1') selected @endif>Januari</option>
-								<option value="2" @if($upah->bulan == '2') selected @endif>Februari</option>
-								<option value="3" @if($upah->bulan == '3') selected @endif>Maret</option>
-								<option value="4" @if($upah->bulan == '4') selected @endif>April</option>
-								<option value="5" @if($upah->bulan == '5') selected @endif>Mei</option>
-								<option value="6" @if($upah->bulan == '6') selected @endif>Juni</option>
-								<option value="7" @if($upah->bulan == '7') selected @endif>Juli</option>
-								<option value="8" @if($upah->bulan == '8') selected @endif>Agustus</option>
-								<option value="9" @if($upah->bulan == '9') selected @endif>September</option>
-								<option value="10" @if($upah->bulan == '10') selected @endif>Oktober</option>
-								<option value="11" @if($upah->bulan == '11') selected @endif>November</option>
-								<option value="12" @if($upah->bulan == '12') selected @endif>Desember</option>
+								<option value="1">Januari</option>
+								<option value="2">Februari</option>
+								<option value="3">Maret</option>
+								<option value="4">April</option>
+								<option value="5">Mei</option>
+								<option value="6">Juni</option>
+								<option value="7">Juli</option>
+								<option value="8">Agustus</option>
+								<option value="9">September</option>
+								<option value="10">Oktober</option>
+								<option value="11">November</option>
+								<option value="12">Desember</option>
 							</select>
 							<div id="bulan-nya"></div>
 						</div>
 
 						<label for="tahun" class="col-2 col-form-label">Tahun</label>
 						<div class="col-4">
-							<input class="form-control" type="text" name="tahun" id="tahun" value="{{ $upah->tahun }}">
+							<input class="form-control" type="text" name="tahun" id="tahun" value="{{ date('Y') }}">
 						</div>
 					</div>
 
@@ -83,7 +78,7 @@
 							<select class="form-control kt-select2" name="pegawai" id="pegawai">
 								<option value="">- Pilih Pegawai -</option>
 								@foreach ($pekerja_list as $pekerja)
-									<option value="{{ $pekerja->nopeg }}" @if($pekerja->nopeg == $upah->nopek) selected @endif>{{ $pekerja->nopeg.' - '.$pekerja->nama.' - '.pekerja_status($pekerja->status) }}</option>
+									<option value="{{ $pekerja->nopeg }}">{{ $pekerja->nopeg.' - '.$pekerja->nama.' - '.pekerja_status($pekerja->status) }}</option>
 								@endforeach
 							</select>
 							<div id="pegawai-nya"></div>
@@ -96,7 +91,7 @@
 							<select class="form-control kt-select2" name="aard" id="aard">
 								<option value="">- Pilih AARD -</option>
 								@foreach ($aard_list as $aard)
-									<option value="{{ $aard->kode }}" @if($aard->kode == $upah->aard) selected @endif>{{ $aard->kode.' - '.$aard->nama }}</option>
+									<option value="{{ $aard->kode }}">{{ $aard->kode.' - '.$aard->nama }}</option>
 								@endforeach
 							</select>
 							<div id="aard-nya"></div>
@@ -106,21 +101,21 @@
 					<div class="form-group row">
 						<label for="nilai" class="col-2 col-form-label">Jumlah Cicilan</label>
 						<div class="col-10">
-							<input class="form-control" type="number" name="jumlah_cicilan" id="jumlah_cicilan" value="{{ $upah->jmlcc }}">
+							<input class="form-control" type="text" name="jumlah_cicilan" id="jumlah_cicilan">
 						</div>
 					</div>
 
 					<div class="form-group row">
 						<label for="nilai" class="col-2 col-form-label">Cicilan</label>
 						<div class="col-10">
-							<input class="form-control" type="number" name="cicilan" id="cicilan" value="{{ $upah->ccl }}">
+							<input class="form-control" type="text" name="cicilan" id="cicilan">
 						</div>
 					</div>
 
 					<div class="form-group row">
 						<label for="nilai" class="col-2 col-form-label">Nilai</label>
 						<div class="col-10">
-							<input class="form-control" type="number" name="nilai" id="nilai" value="{{ $upah->nilai }}">
+							<input class="form-control" type="text" name="nilai" id="nilai">
 						</div>
 					</div>
 
@@ -143,7 +138,7 @@
 @endsection
 
 @section('scripts')
-{!! JsValidator::formRequest('App\Http\Requests\UpahMasterUpdate', '#formUpahMaster') !!}
+{!! JsValidator::formRequest('App\Http\Requests\ThrMasterStore', '#formThrMaster') !!}
 
 <script type="text/javascript">
 	$(document).ready(function () {
@@ -151,7 +146,7 @@
 			$(this).valid();
 		});
 
-		$("#formUpahMaster").on('submit', function(){
+		$("#formThrMaster").on('submit', function(){
 			if ($('#bulan-error').length){
 				$("#bulan-error").insertAfter("#bulan-nya");
 			}
