7bd500415ec962a7eef4828b06b60964eeede999
mulyono <37521871+mulyonomuiz63@users.noreply.github.com>
2020-06-27 08:05:11 +0700

    edit terminalname

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -45,7 +45,7 @@ class AuthController extends Controller
                         if ($password <> $rsuser->userpw) {
                             return redirect('/error')->with('notif', "Your password not allowed...");
                         }else{
-                            if (($sUserHost<>"%" and $UserIPAddress) <> $sUserHost) {
+                            if ($sUserHost<>"%" and $UserIPAddress <> $sUserHost) {
                                 return redirect('/error')->with('notif', "You are not allowed to access from there...");
                             }else{
                                 $rsUserLogin = DB::select("select userid from userlogin where terminal='$GetTerminalName' and userid='$loginid'");
