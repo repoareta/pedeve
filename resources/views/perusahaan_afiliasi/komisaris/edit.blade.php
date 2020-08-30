ca3eb0120377a724082862eb30e389eb7b5927a1
erkalodon <andhika.ragilkesuma@gmail.com>
2020-08-31 00:29:23 +0700

    Perusahaan Afiliasi Insert

---
+++ resources/views/perusahaan_afiliasi/komisaris/edit.blade.php
@@ -0,0 +1,46 @@
+{{-- KOMISARIS START --}}
+<div class="kt-portlet__head kt-portlet__head">
+    <div class="kt-portlet__head-label">
+        <span class="kt-portlet__head-icon">
+            <i class="kt-font-brand flaticon2-line-chart"></i>
+        </span>
+        <h3 class="kt-portlet__head-title">
+            Komisaris
+        </h3>
+        
+        <div class="kt-portlet__head-actions" style="font-size: 2rem;">
+            <a href="#" id="openDetail">
+                <span class="kt-font-success" data-toggle="kt-tooltip" data-placement="top" title="Tambah Data">
+                    <i class="fas fa-plus-circle"></i>
+                </span>
+            </a>
+
+            <span class="kt-font-warning pointer-link" id="editRow" data-toggle="kt-tooltip" data-placement="top" title="Ubah Data">
+                <i class="fas fa-edit"></i>
+            </span>
+
+            <span class="kt-font-danger pointer-link" id="deleteRow" data-toggle="kt-tooltip" data-placement="top" title="Hapus Data">
+                <i class="fas fa-times-circle"></i>
+            </span>
+        </div>
+    </div>
+    <div class="kt-portlet__head-toolbar">
+        <div class="kt-portlet__head-wrapper">
+        </div>
+    </div>
+</div>
+<div class="kt-portlet__body">
+    <table class="table table-striped table-bordered table-hover table-checkable" id="kt_table">
+        <thead class="thead-light">
+            <tr>
+                <th></th>
+                <th>Nama</th>
+                <th>TMT Dinas</th>
+                <th>Akhi Masa Dinas</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+</div>
+{{-- KOMISARIS END --}}
