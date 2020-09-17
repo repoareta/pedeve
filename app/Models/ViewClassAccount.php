96c0fbd764a49b821307e8501e28e33aa2982474
erkalodon <andhika.ragilkesuma@gmail.com>
2020-09-17 16:54:00 +0700

    Update Export CALK Report

--- app/Models/ViewClassAccount.php
+++ app/Models/ViewClassAccount.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class ViewClassAccount extends Model
 {
     protected $table = 'v_class_account';
+
+    public function sub_class_account()
+    {
+        return $this->hasMany('App\Models\ViewSubClassAccount', 'urutan', 'urutan');
+    }
+
+    public function neraca()
+    {
+        return $this->hasMany('App\Models\ViewNeraca', 'sub_akun', 'batas_awal');
+    }
 }
--- app/Models/ViewNeraca.php
+++ app/Models/ViewNeraca.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class ViewNeraca extends Model
 {
     protected $table = 'v_neraca';
+
+    public function andet()
+    {
+        return $this->belongsTo('App\Models\ViewAndet', 'sandi', 'sandi');
+    }
 }
