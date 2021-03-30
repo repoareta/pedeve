f2678920b6aeb466aec1d364ef9e42d888f13ba6
mulyonomuiz63 <mulyonomuiz63@gmail.com>
2021-03-30 11:56:40 +0700

    edit url

--- resources/views/informasi_saldo/edit.blade.php
+++ resources/views/informasi_saldo/edit.blade.php
@@ -789,7 +789,7 @@ if($('input[type=radio]').is(':checked')) {
 		var nodok = $(this).attr('nodok').split("/").join("-");
 		var nourut = $(this).attr('nourut');
 			$.ajax({
-				url :"{{('perbendaharaan/penerimaan_kas/editdetail')}}"+ '/' +nodok+ '/' +nourut,
+				url :"{{url('perbendaharaan/penerimaan_kas/editdetail')}}"+ '/' +nodok+ '/' +nourut,
 				type : 'get',
 				dataType:"json",
 				headers: {
